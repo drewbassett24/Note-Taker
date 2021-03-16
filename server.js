bff2ba399596281b731d9854d2a72d760fcbed11
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-03-16 20:17:39 +0000

    Loaded ReadMe Template; created rooutes for notes and index html

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const noteData = require('');
-var uniqueId = require('uniqueId');
-
+const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(express.static('public'));
 
+// Use Get * here.
 app.get('/', function(req, res) {
-    res.sendFile(path.join(_dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+    
 });
 
 //Use json to transfer between front and back ends 
@@ -17,17 +17,16 @@ app.use(express.urlencoded({extended: true}));
 
 // Gets request and directs user to homepage
 app.get('/', function(req, res) {
-    res.sendFile(path.join(_dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 // Directs user to 'notes' page
 app.get('/notes', function(req, res) {
-    res.sendFile(path.join(_dirname, "./public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on: http//localhost ${PORT}`);
 });
 
 
-server.listen(PORT, () => {
-    console.log('Server listening on: http//localhost${PORT}');
-});
-
-const PORT = process.env.PORT || 8080;
