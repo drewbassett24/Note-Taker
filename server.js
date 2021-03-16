798324cf3b9286d86735c1034702093615deb288
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-03-16 14:19:15 +0000

    Server created

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
-const http = require ('http');
-const PORT = 8080;
-const handleRequest = (request, response) => {
-    response.end();
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const noteData = require('');
+var uniqueId = require('uniqueId');
 
-};
-const server = httpcreateServer(handleRequest);
+const app = express();
+app.use(express.static('public'));
 
-server.listen (PORT, () => {
+app.get('/', function(req, res) {
+    res.sendFile(path.join(_dirname, "./public/index.html"));
+});
+
+//Use json to transfer between front and back ends 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Gets request and directs user to homepage
+app.get('/', function(req, res) {
+    res.sendFile(path.join(_dirname, "./public/index.html"));
+});
+
+// Directs user to 'notes' page
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(_dirname, "./public/notes.html"));
+});
+
+
+server.listen(PORT, () => {
     console.log('Server listening on: http//localhost${PORT}');
 });
+
+const PORT = process.env.PORT || 8080;
