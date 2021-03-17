4ec7f0f6a02d5dab5db7da727d178c90a938a1d5
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-03-17 10:26:40 +0000

    Installed uniqid and added a function to use it to give notes unique ids.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const uniqid = require ('uniqid');
+
 const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(express.static('public'));
@@ -29,4 +31,24 @@ app.listen(PORT, () => {
     console.log(`Server listening on: http//localhost ${PORT}`);
 });
 
+// Create api routes here
+app.get('/api/notes', function(req, res) {
+    res.json(noteData);
+});
 
+// Push the note to the page
+app.post("/api/notes", (req, res) => {
+
+    // Give each note a unique id
+    var postNotes = req.body;
+    postNotes.id = uniqid();
+    console.log(req.body.id: " + postNotes);
+    noteData.push(postNotes);
+    noteAdded(noteData);
+    res.json(noteData);
+
+});
+
+module.exports = function (app) {
+
+}
